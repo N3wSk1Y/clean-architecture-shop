da708175f52c592c5ddb9f7252a0a3dad367de58
Dmitriy Fedenko <miner.fedenko@gmail.com>
2024-06-17 13:40:44 +0300

    (feat & style): добавлен update, delete в book.service, prettying

--- src/core/domains/book/book.service.ts
+++ src/core/domains/book/book.service.ts
@@ -3,6 +3,7 @@ import { UndefinedBookIdError } from "./errors/undefined-book-id.error";
 import { CreateBookDto } from "./dto/create-book.dto";
 import { AuthorService } from "../author/author.service";
 import { PublicBookModel } from "./models/public-book.model";
+import { UpdateBookDto } from "./dto/update-book.dto";
 
 export class BookService {
     private bookRepository: BookRepository;
@@ -27,5 +28,18 @@ export class BookService {
     public async create(authorId: number, bookData: CreateBookDto): Promise<PublicBookModel> {
         const author = await this.authorService.getById(authorId);
         return await this.bookRepository.create(author.id, bookData);
+    }
+
+    public async update(bookId: number, bookData: UpdateBookDto): Promise<PublicBookModel> {
+        return await this.bookRepository.update(bookId, bookData);
+    }
+
+    public async updateAuthor(bookId: number, authorId: number): Promise<PublicBookModel> {
+        const author = await this.authorService.getById(authorId);
+        return await this.bookRepository.changeAuthor(bookId, author.id);
+    }
+
+    public async delete(bookId: number): Promise<PublicBookModel> {
+        return await this.bookRepository.delete(bookId);
     }
 }
